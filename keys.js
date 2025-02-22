afbd88dc9250e6a015bc1a7eb32d57a8837b5281
Meghavarshini M <141480398+meghaaaav@users.noreply.github.com>
2025-02-22 19:56:22 +0530

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://meghxmohan:achuachu@cluster0.o0fdz.mongodb.net/test?retryWrites=true&w=majority&tls=true",
-    jwtkey:"313572c1a9bd13133767f887c175cc654313cffe54e2e2d04f75429928222b78y"
+    mongoURI: "yourmongoURI",
+    jwtkey:"yoursecretjwtkey"
 };
